322032fa7c50083eeb81ca86a85574fcfdad6f07
Ekikere-abasi Ekere <ekisnzaki@gmail.com>
2023-10-21 16:23:29 +0100

    Added images

--- src/pages/Discoveries.jsx
+++ src/pages/Discoveries.jsx
@@ -1,23 +1,48 @@
 import { Link } from "react-router-dom"
 import Layout from "../Components/global/Layout"
+import { useState } from "react"
 // import image from '../assets/noah-buscher-x8ZStukS2PM-unsplash.jpg'
 
 function DiscoveryPage() {
 
+    const [discoveries, setDiscoveries] = useState([
+        {
+            id: 1,
+            title: "Discover with us together we build a better ecosystem",
+            content: " A substantial discovery often brings a sense of joy and wonder to our lives. It could be stumbling upon a hidden gem in your local neighborhood, finding an unexpected act of kindness from a stranger, or uncovering a new passion or talent you never knew you had. These discoveries remind us of the beauty and surprises that life has to offer, adding a touch of excitement and gratitude to our everyday experiences. They are the small but meaningful moments that make life more enjoyable and fulfilling. Embracing the spirit of curiosity and exploration can lead to many nice discoveries that brighten our days.",
+            image: "/images/eco.jpg"
+        },
+        {
+            id: 13,
+            title: "Saving Eccosytem",
+            content: " A substantial discovery often brings a sense of joy and wonder to our lives. It could be stumbling upon a hidden gem in your local neighborhood, finding an unexpected act of kindness from a stranger, or uncovering a new passion or talent you never knew you had. These discoveries remind us of the beauty and surprises that life has to offer, adding a touch of excitement and gratitude to our everyday experiences. They are the small but meaningful moments that make life more enjoyable and fulfilling. Embracing the spirit of curiosity and exploration can lead to many nice discoveries that brighten our days.",
+            image: "/images/eco2.jpeg"
+        },
+        {
+            id: 31,
+            title: "Saving Eccosytem Part 2",
+            content: " A substantial discovery often brings a sense of joy and wonder to our lives. It could be stumbling upon a hidden gem in your local neighborhood, finding an unexpected act of kindness from a stranger, or uncovering a new passion or talent you never knew you had. These discoveries remind us of the beauty and surprises that life has to offer, adding a touch of excitement and gratitude to our everyday experiences. They are the small but meaningful moments that make life more enjoyable and fulfilling. Embracing the spirit of curiosity and exploration can lead to many nice discoveries that brighten our days.",
+            image: "/images/eco3.jpeg"
+        }
 
+    ])
     return (
         <Layout>
-            <div>
-                <DiscoveryFrom />
-                <Discoveries />
+            <div className="space-y-10">
+                <h3 className="text-4xl">Discover</h3>
+                <DiscoveryForm setDiscoveries={setDiscoveries} />
+                <Discoveries discoveries={discoveries} />
             </div>
         </Layout>
     )
 }
 
-const DiscoveryFrom = () => {
+const DiscoveryForm = ({setDiscoveries}) => {
+    // const onSubmit = ()=>{
+    //     setDiscoveries((prev)=>)
+    // }
     return (<div>
-        <h3></h3>
+
         <form action="#" >
             <div>
                 <label htmlFor="discovery" className="block my-3 text-2xl font-medium leading-6 text-white">
@@ -30,36 +55,16 @@ const DiscoveryFrom = () => {
     </div>)
 }
 
-const Discoveries = () => {
-    const discoveries = [
-        {
-            id: 1,
-            title:"Discover with us together we build a better ecosystem",
-            content: " A substantial discovery often brings a sense of joy and wonder to our lives. It could be stumbling upon a hidden gem in your local neighborhood, finding an unexpected act of kindness from a stranger, or uncovering a new passion or talent you never knew you had. These discoveries remind us of the beauty and surprises that life has to offer, adding a touch of excitement and gratitude to our everyday experiences. They are the small but meaningful moments that make life more enjoyable and fulfilling. Embracing the spirit of curiosity and exploration can lead to many nice discoveries that brighten our days.",
-            image: "/images/eco.jpg"
-        },
-        {
-            id: 13,
-            title:"Saving Eccosytem",
-            content: " A substantial discovery often brings a sense of joy and wonder to our lives. It could be stumbling upon a hidden gem in your local neighborhood, finding an unexpected act of kindness from a stranger, or uncovering a new passion or talent you never knew you had. These discoveries remind us of the beauty and surprises that life has to offer, adding a touch of excitement and gratitude to our everyday experiences. They are the small but meaningful moments that make life more enjoyable and fulfilling. Embracing the spirit of curiosity and exploration can lead to many nice discoveries that brighten our days.",
-            image: "/images/eco2.jpeg"
-        },
-        {
-            id: 31,
-            title:"Saving Eccosytem Part 2",
-            content: " A substantial discovery often brings a sense of joy and wonder to our lives. It could be stumbling upon a hidden gem in your local neighborhood, finding an unexpected act of kindness from a stranger, or uncovering a new passion or talent you never knew you had. These discoveries remind us of the beauty and surprises that life has to offer, adding a touch of excitement and gratitude to our everyday experiences. They are the small but meaningful moments that make life more enjoyable and fulfilling. Embracing the spirit of curiosity and exploration can lead to many nice discoveries that brighten our days.",
-            image: "/images/eco3.jpeg"
-        }
+const Discoveries = ({ discoveries }) => {
 
-    ]
     return <div className="grid grid-cols-2 gap-5 my-6">
         {
             discoveries.map(({ content, id, image, title }) => {
                 return (
                     <div key={id} className="overflow-hidden border rounded-md border-primary">
-                       <div className="max-h-[300px] overflow-hidden">
-                       <img src={image} alt={id} className="w-[600px] " />
-                       </div>
+                        <div className="max-h-[300px] overflow-hidden">
+                            <img src={image} alt={id} className="w-[600px] " />
+                        </div>
                         <div className="px-2 py-3">
                             <h4 className="my-3 text-3xl fontbold'">{title}</h4>
                             <span>{content.slice(0, 330) + "..."}</span>
